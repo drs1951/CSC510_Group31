51304d8c8679a0dd2208cc982163ef4a654164f5
Kohav-Yadav <kohavyadav261@gmail.com>
2023-11-27 19:44:30 -0500

    Logo, home page UI changed .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pop:false,
+      pop: false,
     };
   }
   logOut = () => {
@@ -37,24 +37,24 @@ class Navbar extends React.Component {
     this.props.dispatch(searchUsers(searchText));
   };
 
-  componentDidUpdate(prevProps){
-    const {job} = this.props;
+  componentDidUpdate(prevProps) {
+    const { job } = this.props;
     console.log(job)
     if (this.props.job !== prevProps.job) {
-      const {job} = this.props;
-      
-      if(job){
+      const { job } = this.props;
+
+      if (job) {
         for (const jb of job) {
           console.log("Runnign")
           const utc2 = new Date();
           const jobExpiredDate = new Date(jb.dateexpired);
           const twoDaysBefore = new Date(jobExpiredDate);
-          
+
           twoDaysBefore.setDate(jobExpiredDate.getDate() - 2);
-        
+
           if (twoDaysBefore.getTime() <= utc2.getTime() || jb.quantity < 10) {
             this.setState({
-              pop:true
+              pop: true
             })
             break; // Exit the loop when the condition is met
           }
@@ -66,7 +66,7 @@ class Navbar extends React.Component {
   render() {
     const { auth } = this.props;
     const { user } = this.props.auth;
-    const {job} = this.props;
+    const { job } = this.props;
 
     let utc2 = new Date();
 
@@ -80,57 +80,57 @@ class Navbar extends React.Component {
         >
           <Link to="/">
             <img
-              
-              alt="logo"
-              src="/images/WhatsApp Image 2021-12-04 at 4.31.50 PM.jpeg"
-              style={{ height: "50px" }}
+
+              alt='logo'
+              src="/images/Logo3.11.png"
+              style={{ height: "110px" }}
             />
           </Link>
 
-          
+
         </div>
         {auth.isLoggedIn && (
           <div className="header__middle" style={{ marginLeft: "20px" }}>
             <div className="header__option ">
               <Link to="/menu" title="Menu">
-                <RestaurantMenuIcon fontSize="large"/>
+                <RestaurantMenuIcon fontSize="large" />
               </Link>
             </div>
-            
-              <div className="header__option ">
-                <Link to="/goal" title="Inventory">
-                  <ListAltIcon fontSize="large" />
-                </Link>
-              </div>
-            
-           
-              <div className="header__option ">
-                <Link to="/update" title="Inventory List">
-                  <InventoryIcon fontSize="large" />
-                </Link>
-              </div>
-            
-            
-              <div className="header__option ">
-                <Link to="/order" title="Order">
-                  <ReceiptIcon fontSize="large" />
-                </Link>
-              </div>
 
-              <div className="header__option ">
-                <Link to="/history" title="Analytics">
-                  <TimelineIcon fontSize="large" />
-                </Link>
-              </div>
+            <div className="header__option ">
+              <Link to="/goal" title="Inventory">
+                <ListAltIcon fontSize="large" />
+              </Link>
+            </div>
 
-              <div className="header__option ">
-                <Link to="/notification" title="Notification">
-                  <NotificationsActiveIcon fontSize="large" color={this.state.pop?"secondary":""} className={this.state.pop?`constant-tilt-shake`:""}/>
-                </Link>
-              </div>
 
-              
-            
+            <div className="header__option ">
+              <Link to="/update" title="Inventory List">
+                <InventoryIcon fontSize="large" />
+              </Link>
+            </div>
+
+
+            <div className="header__option ">
+              <Link to="/order" title="Order">
+                <ReceiptIcon fontSize="large" />
+              </Link>
+            </div>
+
+            <div className="header__option ">
+              <Link to="/history" title="Analytics">
+                <TimelineIcon fontSize="large" />
+              </Link>
+            </div>
+
+            <div className="header__option ">
+              <Link to="/notification" title="Notification">
+                <NotificationsActiveIcon fontSize="large" color={this.state.pop ? "secondary" : ""} className={this.state.pop ? `constant-tilt-shake` : ""} />
+              </Link>
+            </div>
+
+
+
 
           </div>
         )}
@@ -141,22 +141,24 @@ class Navbar extends React.Component {
                 <Link to="/settings">
                   <img
                     // src="https://cdn-icons.flaticon.com/png/512/668/premium/668709.png?token=exp=1636045281~hmac=01dc4c9a3c91ca3c5bae9c160e2fb7c6"
-                    src={`https://ui-avatars.com/api/?name=${firstLetter}`}
+
+                    src={`https://ui-avatars.com/api/?background=fb5607 &&  name=${firstLetter}`}
                     alt="user-dp"
                     id="user-dp"
-                    style={{ marginLeft: "0px" }}
+
+                    style={{ marginLeft: "0px", height: "50px" }}
                   />
                 </Link>
                 <span
                   style={{
-                    color: "gray",
-                    marginLeft: "10px",
+                    color: "black",
+                    marginRight: "10px",
                     fontWeight: "bolder",
                   }}
                 >
                   {auth.user.name}
                 </span>
-                
+
               </div>
             )}
 
@@ -167,8 +169,8 @@ class Navbar extends React.Component {
                     <Link
                       to="/login"
                       style={{
-                        color: "gray",
-                        marginLeft: "10px",
+                        color: "black",
+                        marginLeft: "20px",
                         fontWeight: "bolder",
                       }}
                     >
@@ -179,10 +181,11 @@ class Navbar extends React.Component {
 
                 {auth.isLoggedIn && (
                   <li
+                    className="Hover"
                     onClick={this.logOut}
                     style={{
-                      color: "gray",
-                      marginLeft: "0px",
+                      color: "black",
+                      marginRight: "50px",
                       fontWeight: "bolder",
                     }}
                   >
@@ -194,7 +197,7 @@ class Navbar extends React.Component {
                     <Link
                       to="/signup"
                       style={{
-                        color: "gray",
+                        color: "Black",
                         marginLeft: "10px",
                         fontWeight: "bolder",
                       }}
@@ -207,7 +210,7 @@ class Navbar extends React.Component {
             </div>
           </div>
         </div>
-      </nav>
+      </nav >
     );
   }
 }
@@ -216,7 +219,7 @@ export { firstLetter };
 function mapStateToProps(state) {
   return {
     auth: state.auth,
-  
+
   };
 }
 
